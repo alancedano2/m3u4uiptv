cf44be4ba3bbbec9dde935aa8a4d3a2cb3d4872d
alancedano2 <supernatura94@yahoo.com>
2025-04-17 14:19:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-module.exports = (req, res) => {
-  const { pathname } = req.url;
+const moment = require('moment');
 
-  if (pathname === '/playlist.m3u') {
-    res.setHeader('Content-Type', 'audio/x-mpegurl');
-    res.send(`
-#EXTM3U
-#EXTINF:-1, Canal 1
-https://miservidor.com/canal1
-#EXTINF:-1, Canal 2
-https://miservidor.com/canal2
-    `);
-  } else if (pathname === '/epg.xml') {
-    res.setHeader('Content-Type', 'application/xml');
-    res.send(`
-<?xml version="1.0" encoding="UTF-8"?>
-<tv>
-  <channel id="canal1">
-    <display-name>Canal 1</display-name>
-  </channel>
-  <programme start="20250418080000 +0000" stop="20250418083000 +0000" channel="canal1">
-    <title>Noticiero</title>
-    <desc>Últimas noticias del día</desc>
-  </programme>
-  <channel id="canal2">
-    <display-name>Canal 2</display-name>
-  </channel>
-  <programme start="20250418083000 +0000" stop="20250418100000 +0000" channel="canal2">
-    <title>Deportes</title>
-    <desc>Resumen deportivo</desc>
-  </programme>
-</tv>
-    `);
-  } else {
-    res.status(404).send('Not Found');
+const channels = {
+  // Tu objeto de programación de canales aquí...
+};
+
+function getCurrentProgram(channelName) {
+  const channel = channels[channelName];
+  const currentTime = moment().format('HH:mm');
+  const currentDay = moment().format('dddd').toLowerCase();
+
+  let schedule = channel.schedule.weekdays;
+  if (currentDay === 'saturday' || currentDay === 'sunday') {
+    schedule = channel.schedule.weekends;
   }
+
+  for (let i = 0; i < schedule.length; i++) {
+    const show = schedule[i];
+    const startTime = moment(show.start, 'HH:mm');
+    const endTime = moment(show.end, 'HH:mm');
+    const currentTimeMoment = moment(currentTime, 'HH:mm');
+
+    if (currentTimeMoment.isBetween(startTime, endTime, null, '[)')) {
+      return {
+        show: show.show,
+        m3u8: channel.m3u8,
+        logo: channel.logo,
+      };
+    }
+  }
+
+  return {
+    show: 'OFF AIR',
+    m3u8: channel.m3u8,
+    logo: channel.logo,
+  };
+}
+
+module.exports = (req, res) => {
+  const channelName = req.query.channel || 'KQ105 Radio'; // Se toma el canal como parámetro
+  const currentProgram = getCurrentProgram(channelName);
+  res.status(200).json(currentProgram);
 };
