bdcaec555125551abfd08b6ab34e881a26a8026e
alancedano2 <supernatura94@yahoo.com>
2025-04-17 15:08:03 -0400

    Update m3u.ts

--- api/m3u.ts
+++ api/m3u.ts
@@ -1,56 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// api/m3u.ts
 
-const channels = [
-  {
-    id: 'KQ105Radio',
-    name: 'KQ105 Radio',
-    logo: 'https://bloximages.chicago2.vip.townnews.com/kq105.com/content/tncms/custom/image/23c304fa-a5bb-11ee-aed9-ab40ab64f7be.png?resize=990%2C284',
-    stream: 'https://streaming-kq105radio.com/live',
-    schedule: [
-      { start: '20250417T000000Z', stop: '20250417T060000Z', title: 'Carlos Gabriel', desc: 'Carlos Gabriel en vivo.' },
-      { start: '20250417T060000Z', stop: '20250417T100000Z', title: 'Héctor Ortiz', desc: 'Programa en vivo con Héctor Ortiz.' },
-      { start: '20250417T100000Z', stop: '20250417T150000Z', title: 'Alex Díaz', desc: 'Música y entretenimiento con Alex Díaz.' },
-    ],
-  },
-  {
-    id: 'KQ105TV',
-    name: 'KQ105 TV',
-    logo: 'https://bloximages.chicago2.vip.townnews.com/kq105.com/content/tncms/custom/image/23c304fa-a5bb-11ee-aed9-ab40ab64f7be.png?resize=990%2C284',
-    stream: 'https://streaming-kq105tv.com/live',
-    schedule: [
-      { start: '20250417T000000Z', stop: '20250417T080000Z', title: 'OFF AIR', desc: 'Canal fuera de aire.' },
-      { start: '20250417T080000Z', stop: '20250417T100000Z', title: 'KQ Hits', desc: 'Música KQ Hits.' },
-      { start: '20250417T100000Z', stop: '20250417T120000Z', title: 'KQ Playlists', desc: 'Playlist de música en vivo.' },
-    ],
-  },
-  {
-    id: 'NetflixEventos',
-    name: 'Netflix Eventos',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1200px-Netflix_2015_N_logo.svg.png',
-    stream: 'https://streaming-netflixeventos.com/live',
-    schedule: [
-      { start: '20250417T000000Z', stop: '20250417T200000Z', title: 'OFF AIR', desc: 'Canal fuera de aire.' },
-      { start: '20250417T200000Z', stop: '20250417T230000Z', title: 'WWE RAW', desc: 'Evento WWE RAW en vivo.' },
-    ],
-  },
-];
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-const generateM3U = (channels) => {
-  let m3uContent = '#EXTM3U\n';
-
-  channels.forEach(channel => {
-    channel.schedule.forEach(program => {
-      m3uContent += `#EXTINF:-1 tvg-id="${channel.id}" tvg-name="${channel.name}" tvg-logo="${channel.logo}",${program.title}\n`;
-      m3uContent += `${channel.stream}\n`;
-    });
-  });
-
-  return m3uContent;
-};
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const m3uContent = generateM3U(channels);
-
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Content-Type', 'application/x-mpegURL');
+
+  const m3uContent = `#EXTM3U
+  #EXTINF:-1 tvg-id="KQ105Radio" tvg-name="KQ105 Radio" tvg-logo="https://bloximages.chicago2.vip.townnews.com/kq105.com/content/tncms/custom/image/23c304fa-a5bb-11ee-aed9-ab40ab64f7be.png?resize=990%2C284" group-title="Radio",KQ105 Radio
+  https://televicentro.streamguys1.com/wkaqfm/playlist.m3u8?listeningSessionID=6557d57d3d1a82ad_42914703_GIGC3M1Z_MjE2LjI0Ni42MC4yNg!!_0000003qjCt&downloadSessionID=0&key=88245e45637ae0b9d1b336e48bc005b61ac7df65286a5f69e1b33f2deb57bf9b&aw_0_1st.playerId=kq105&source=kq105.com&us_privacy=1YNY&clientType=web&callLetters=WKAQ-FM&devicename=web-desktop&stationid=1846&dist=kq105.com&subscription_type=free&aw_0_1st.version=1.0_html5&aw_0_1st.playerid=kq105_floating_player
+  
+  #EXTINF:-1 tvg-id="KQ105TV" tvg-name="KQ105 TV" tvg-logo="https://bloximages.chicago2.vip.townnews.com/kq105.com/content/tncms/custom/image/23c304fa-a5bb-11ee-aed9-ab40ab64f7be.png?resize=990%2C284" group-title="TV",KQ105 TV
+  https://ssh101stream.ssh101.com/akamaissh101/ssh101/kq105/playlist.m3u8
+  
+  #EXTINF:-1 tvg-id="NetflixEventos" tvg-name="Netflix Eventos" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1200px-Netflix_2015_N_logo.svg.png" group-title="Eventos",Netflix Eventos
+  https://giatv.bozztv.com/giatv/giatv-PPVDeportes2/PPVDeportes2/playlist.m3u8`;
+
   res.status(200).send(m3uContent);
-};
+}
